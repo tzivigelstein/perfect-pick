005c3b7c06dcf41e1585052dff5982c162f88792
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2024-05-31 16:43:05 -0300

    update header styling and structure

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,21 +1,37 @@
-import { MagicWand } from "@/components/Icons"
+import { CurvyArrowBack, MagicWand } from "@/components/Icons"
 
 interface Props {
   reset: () => void
+  hasData: boolean
 }
 
-export default function Header({ reset }: Props) {
+export default function Header({ reset, hasData }: Props) {
   return (
     <header className="flex w-full justify-between items-center">
-      <h1 className="w-full" style={{ fontFamily: "var(--font-dm-sans)" }}>
+      <h1
+        className="w-full flex gap-1"
+        style={{ fontFamily: "var(--font-dm-sans)" }}
+      >
         <button className="font-semibold text-xl" onClick={reset}>
           Perfect pick 😉
         </button>
+        <span className="text-teal-100 px-4 py-2 rounded-lg flex gap-1 items-center whitespace-nowrap text-sm font-semibold">
+          AI Powered
+          <MagicWand
+            svg={{ width: 21 }}
+            path={{ className: "fill-teal-100" }}
+          />
+        </span>
       </h1>
-      <span className="text-purple-900 px-4 py-2 rounded-lg flex gap-1 items-center whitespace-nowrap text-sm font-semibold">
-        AI Powered
-        <MagicWand fill="rgb(88 28 135 / var(--tw-text-opacity))" width={21} />
-      </span>
+      {hasData && (
+        <button
+          onClick={() => reset()}
+          className="whitespace-nowrap flex gap-2 items-center border-transparent border-2 p-2 rounded-xl hover:border-white"
+        >
+          Try another one
+          <CurvyArrowBack fill="white" />
+        </button>
+      )}
     </header>
   )
 }
