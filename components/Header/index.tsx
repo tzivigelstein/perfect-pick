8bc0358ff2476f98406157dbe519bc84889d3c4f
Tzivi Gelstein <gelsteintzivisantiago@gmail.com>
2024-06-11 17:29:35 -0300

    remove ai powered badge

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -9,19 +9,12 @@ export default function Header({ reset, hasData }: Props) {
   return (
     <header className="flex w-full justify-between items-center">
       <h1
-        className="w-full flex gap-1"
+        className="w-full"
         style={{ fontFamily: "var(--font-dm-sans)" }}
       >
         <button className="font-semibold text-xl" onClick={reset}>
           Perfect pick 😉
         </button>
-        <span className="text-teal-100 px-4 py-2 rounded-lg flex gap-1 items-center whitespace-nowrap text-sm font-semibold">
-          AI Powered
-          <MagicWand
-            svg={{ width: 21 }}
-            path={{ className: "fill-teal-100" }}
-          />
-        </span>
       </h1>
       {hasData && (
         <button
